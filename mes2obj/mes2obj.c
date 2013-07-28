a56a9faae6c6aed8bdf76bc04590d61ec1bc1352
zeiban <rstolle@gmail.com>
2013-07-28 17:54:19 -0400

    Added -n sub directory creation depth

--- mes2obj/mes2obj.c
+++ mes2obj/mes2obj.c
@@ -11,14 +11,23 @@
 
 void Usage(void)
 {
-	printf("Usage mes2obj.exe  [-w <wad_dir>] [-o <output_dir>] [-s <search_name>] [-f] [-n]\n");
-	printf("Extracts Defiance meshes and converts them to OBJ & MTL files\n");
-	printf("-w\t Wad directory. eg. c:\\games\\defiance\\live\\wad\n");
-	printf("-o\t (Optional) Directory to output OBJ & MTL files otherwise the current directory is used\n");
-	printf("-s\t (Optional) Only extracts files that have <search_name> in the name\n");
-	printf("-f\t (Optional) Creates a sub directory under the <output_dir> with the name of the WAD file\n");
-	printf("-n\t (Optional) Creates a sub directory under the <output_dir> with the name mesh\n");
-	printf("-h\t Displays this information\n");
+	printf("Extracts Defiance static meshes and converts them to OBJ, MTL, and PNG files\n");
+	printf("Usage: mes2obj.exe  [-w dir] [-o dir] [-s search] [-c] [-f] [-n depth]\n");
+	printf("-w (Required) Wad directory. eg. c:\\games\\defiance\\live\\wad\n");
+	
+	printf("-o (Optional) Directory to output OBJ, MTL & PNG files otherwise the current\n"); 
+	printf("   directory is used.\n");
+
+	printf("-s (Optional) Only extracts files that have \"search\" in the name.\n");
+
+	printf("-f (Optional) Creates a sub directory under the \"-o dir\" with the name of\n");
+	printf("    the WAD file. Can be combined with -n.\n");
+
+	printf("-n (Optional) Creates a sub directory under the \"-o dir\" with the name mesh\n");
+	printf("   If \"depth\" is specified it will create sub directories for strings between\n");
+	printf("   the \"_\" in the mesh name.  Can be combined with -f\n");
+
+	printf("-h Displays this information\n");
 }
 
 int main( int argc, const char* argv[])
@@ -38,7 +47,12 @@ int main( int argc, const char* argv[])
 	uint32_t create_name_dir = 0;
 	char wad_out_dir[256];
 	char basename[256];
+	char name[512];
+	char temp_out_dir[512];
 	char full_out_dir[512];
+	char * name_end = NULL;
+	int32_t name_tok_level = -1;
+	char * token = NULL, * next_token;
 
 	printf("Defiance Tools Static Mesh Extraction Utility by Zeiban v%d.%d.%d%s\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_SUFFIX);
 
@@ -58,7 +72,9 @@ int main( int argc, const char* argv[])
 		}  else if(strcmp(argv[i],"-f") == 0) {
 			create_wad_dir = 1;
 		}  else if(strcmp(argv[i],"-n") == 0) {
-			create_name_dir = 1;
+			if(argc>i) { 
+				name_tok_level = strtol(argv[++i], NULL, 10);
+			}
 		}  else if(strcmp(argv[i],"-h") == 0) {
 			Usage();
 			return 1;
@@ -75,9 +91,27 @@ int main( int argc, const char* argv[])
 		out_dir = ".";
 	}
 
-	printf("Input: %s\n", wad_dir);
+	printf("Input WAD Directory: %s\n", wad_dir);
 	if(search_name != NULL) {
 		printf("Search String: \"%s\"\n", search_name);
+	}
+
+	printf("Create WAD output directories: ");
+	if(create_wad_dir != 0) {
+		printf("Yes\n");
+	} else {
+		printf("No\n");
+	}
+
+	printf("Create asset name directories: ");
+	if(name_tok_level != -1) {
+		if(name_tok_level == 0) {
+			printf("Yes\n");
+		} else if (name_tok_level > 0) {
+			printf("Yes (depth %d)\n", name_tok_level);
+		}
+	} else {
+		printf("No\n");
 	}
 
 	printf("Output directory: %s\n", out_dir);
@@ -103,23 +137,37 @@ int main( int argc, const char* argv[])
 
 					if(create_wad_dir) {
 						_splitpath_s(wr->filename,NULL,0,NULL,0,basename,sizeof(basename),NULL,0);
-						sprintf_s(wad_out_dir, sizeof(wad_out_dir),"%s\\%s",out_dir,basename);
-						_mkdir(wad_out_dir);
-					} else {
-						strcpy_s(wad_out_dir, sizeof(wad_out_dir), out_dir); 
-					}
-
-					if(create_name_dir) {
-						if(create_wad_dir) {
-							sprintf_s(full_out_dir, sizeof(full_out_dir),"%s\\%s",wad_out_dir,wr->name);
-						} else {
-							sprintf_s(full_out_dir, sizeof(full_out_dir),"%s\\%s",out_dir,wr->name);
-						}
+						sprintf_s(full_out_dir, sizeof(wad_out_dir),"%s\\%s",out_dir,basename);
 						_mkdir(full_out_dir);
 					} else {
-						strcpy_s(full_out_dir, sizeof(full_out_dir), wad_out_dir); 
+						strcpy_s(full_out_dir, sizeof(full_out_dir), out_dir); 
 					}
-	
+					
+					if(name_tok_level > 0) {
+						i = name_tok_level;
+						strcpy_s(name, sizeof(name), wr->name);
+						do {
+							token = strtok_s(i == name_tok_level ? name : NULL, "_", &next_token);
+							if(token != NULL) {
+								name_end = next_token;
+								sprintf_s(temp_out_dir, sizeof(temp_out_dir),"%s\\%s",full_out_dir,token);
+								strcpy_s(full_out_dir, sizeof(full_out_dir), temp_out_dir); 
+								_mkdir(full_out_dir);
+							}
+							i--;
+						} while(i > 0 && token != NULL);
+
+						if(token != NULL) {
+							sprintf_s(temp_out_dir, sizeof(temp_out_dir),"%s\\%s",full_out_dir,name_end);
+							strcpy_s(full_out_dir, sizeof(full_out_dir), temp_out_dir); 
+							_mkdir(full_out_dir);
+						}
+					} else if (name_tok_level == 0){
+						sprintf_s(temp_out_dir, sizeof(temp_out_dir),"%s\\%s",full_out_dir,wr->name);
+						strcpy_s(full_out_dir, sizeof(full_out_dir), temp_out_dir); 
+						_mkdir(full_out_dir);
+					}
+
 					printf("0x%08X %s ", EndianSwap(wr->id), wr->name);
 
 					if(WadWriteMesToObj(&wd, wr, full_out_dir) != 0) {
